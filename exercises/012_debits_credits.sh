0417cc7547a65cf5d1e223dea217274ce0e8f92e
Paolo Flores <pflores.fisi22@gmail.com>
2024-12-29 10:05:45 -0500

    add exercise 012_debits_credits

--- exercises/012_debits_credits.sh
+++ exercises/012_debits_credits.sh
@@ -18,6 +18,8 @@ source ./tools/tb_function.sh
 BANK=1200
 CUSTOMER=1201
 
+# Note: the following operations are performed from the bank's perspective.
+
 tb "create_accounts id=$BANK code=10 ledger=120,
                     id=$CUSTOMER code=10 ledger=120;"
 
@@ -26,7 +28,16 @@ tb "create_transfers id=12000 debit_account_id=$BANK credit_account_id=$CUSTOMER
 
 # Now, the customer wants to withdraw $20 from the bank.
 # Which account should be debited and which should be credited?
-tb "create_transfers id=12001 debit_account_id=${???} credit_account_id=${???} amount=20 ledger=120 code=10;"
+tb "create_transfers id=12001 debit_account_id=$CUSTOMER credit_account_id=$BANK amount=20 ledger=120 code=10;"
+
+# Explanation
+
+# When the customer deposits money into the bank, the bank assets (which is a normal debit account)
+# should increase, that is, should be debited. On the other hand, part of the customer assets now
+# belong to the bank, so the customer assets should decrease, that is, should be credited.
+
+# The second example is the opposite. Customer assets should increase in $20, that is, should be
+# debited. The bank assets should decrease in $20, that is, should be credited.
 
 # For developers who are new to accounting, debits and credits can be a bit confusing.
 # You can read more debits and credits in our primer on financial accounting:
