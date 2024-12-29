e61faab0920259379afcf422f6d1c8c572177292
Paolo Flores <pflores.fisi22@gmail.com>
2024-12-28 19:28:31 -0500

    add exercise 000_download

--- exercises/000_download.sh
+++ exercises/000_download.sh
@@ -8,7 +8,7 @@
 #curl -Lo tigerbeetle.zip https://mac.tigerbeetle.com && unzip tigerbeetle.zip 
 
 # Linux
-#curl -Lo tigerbeetle.zip https://linux.tigerbeetle.com && unzip tigerbeetle.zip
+curl -Lo tigerbeetle.zip https://linux.tigerbeetle.com && unzip tigerbeetle.zip
 
 # Windows
 #powershell -command "curl.exe -Lo tigerbeetle.zip https://windows.tigerbeetle.com; Expand-Archive tigerbeetle.zip .; .\tigerbeetle version"
