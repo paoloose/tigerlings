39fba728ea422cba5c70759de0579b0d4561bc4e
Paolo Flores <pflores.fisi22@gmail.com>
2024-12-28 19:41:04 -0500

    add exercise 003_create_account

--- exercises/003_create_account.sh
+++ exercises/003_create_account.sh
@@ -11,7 +11,9 @@
 # To connect to TigerBeetle, we normally use one of the client libraries.
 # However, for these exercises we're just going to use the REPL command via the CLI.
 
-./tigerbeetle repl --cluster=0 --addresses=3000 --command="create_account id=300 code=10 ledger=30"
+source ./tools/tb_function.sh
+
+tb "create_accounts id=300 code=10 ledger=30"
 # Uh oh! This command isn't quite right. In TigerBeetle, every request can be batched for 
 # performance so the type of request is always plural. Can you change it to be `create_accounts`?
 
