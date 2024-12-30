57c4a92acd27815ea7c33109411d7cb29d5bcf0c
Paolo Flores <pflores.fisi22@gmail.com>
2024-12-30 00:23:09 -0500

    add exercise 028_balance_conditional_transfers

--- exercises/028_balance_conditional_transfers.sh
+++ exercises/028_balance_conditional_transfers.sh
@@ -10,8 +10,8 @@ source ./tools/tb_function.sh
 # First, we'll create a couple of accounts:
 OPERATOR_ACCOUNT=2800
 CONTROL_ACCOUNT=2801
-SOURCE_ACCOUNT=2810
-DESTINATION_ACCOUNT=2811
+SOURCE_ACCOUNT=2810 # credit account
+DESTINATION_ACCOUNT=2811 # debit account
 
 tb "create_accounts id=$OPERATOR_ACCOUNT code=10 ledger=280,
                     id=$CONTROL_ACCOUNT code=10 ledger=280,
@@ -25,11 +25,11 @@ tb "create_transfers id=28000 debit_account_id=$OPERATOR_ACCOUNT credit_account_
 # The first transfer will only succeed if the SOURCE_ACCOUNT has a balance that exceeds the THRESHOLD_AMOUNT.
 # The second transfers the THRESHOLD_AMOUNT back from the CONTROL_ACCOUNT to the SOURCE_ACCOUNT.
 # Finally, the third makes the actual transfer to the DESTINATION_ACCOUNT.
-THRESHOLD_AMOUNT=200
+THRESHOLD_AMOUNT=100
 TRANSFER_AMOUNT=50
-tb "create_transfers id=28001 debit_account_id=$SOURCE_ACCOUNT credit_account_id=$CONTROL_ACCOUNT amount=$THRESHOLD_AMOUNT ledger=280 code=10 flags=linked,
-                     id=28002 debit_account_id=$CONTROL_ACCOUNT credit_account_id=$SOURCE_ACCOUNT amount=$THRESHOLD_AMOUNT ledger=280 code=10 flags=linked, 
-                     id=28003 debit_account_id=$SOURCE_ACCOUNT credit_account_id=$DESTINATION_ACCOUNT amount=$TRANSFER_AMOUNT ledger=280 code=10;"
+tb "create_transfers id=28005 debit_account_id=$SOURCE_ACCOUNT credit_account_id=$CONTROL_ACCOUNT amount=$THRESHOLD_AMOUNT ledger=280 code=10 flags=linked,
+                     id=28006 debit_account_id=$CONTROL_ACCOUNT credit_account_id=$SOURCE_ACCOUNT amount=$THRESHOLD_AMOUNT ledger=280 code=10 flags=linked,
+                     id=28007 debit_account_id=$SOURCE_ACCOUNT credit_account_id=$DESTINATION_ACCOUNT amount=$TRANSFER_AMOUNT ledger=280 code=10;"
 
 # Uh oh -- this fails because the SOURCE_ACCOUNT's balance doesn't meet the threshold.
 # Can you fix the THRESHOLD_AMOUNT so that the transfers succeed?
